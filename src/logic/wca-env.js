c594b832ebeae96ca12233c90b84caa6d70aadc1
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-09-15 17:45:57 +0200

    Actually enable staging toggle in production.

--- src/logic/wca-env.js
+++ src/logic/wca-env.js
@@ -1,4 +1,6 @@
-export const PRODUCTION = process.env.NODE_ENV === 'production';
+const searchParams = new URLSearchParams(window.location.search);
+export const PRODUCTION =
+  process.env.NODE_ENV === 'production' && !searchParams.has('staging');
 
 export const WCA_ORIGIN = PRODUCTION
   ? 'https://www.worldcubeassociation.org'
