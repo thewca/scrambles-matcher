3a6dd763092c21448938af9da36af455cc278d43
Gregor Billing <gregor.billing.dev@gmail.com>
2022-07-02 12:32:22 +0900

    Revert "Bump react and react-dom (#80)"
    This reverts commit ff1a3f9bc02dbbc2ad5413fe2c0069c8754b97b4.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import "typeface-roboto";
 import "./wca_data/cubing-icons.css";
 import { getOauthTokenIfAny } from "./logic/auth";
@@ -7,6 +7,4 @@ import { getOauthTokenIfAny } from "./logic/auth";
 import App from "./components/App/App";
 
 const userToken = getOauthTokenIfAny();
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App userToken={userToken} />);
+ReactDOM.render(<App userToken={userToken} />, document.getElementById("root"));
