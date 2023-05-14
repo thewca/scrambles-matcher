9e7be6e4ec6e9b4989a2814cf80e5ecf2a32fd2c
Gregor Billing <gregor.billing.dev@gmail.com>
2023-05-14 18:33:48 +0200

    Migrate to React 18 rootRender API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "typeface-roboto";
-import "./wca_data/cubing-icons.css";
-import { getOauthTokenIfAny } from "./logic/auth";
+import React from 'react';
+import { createRoot } from 'react-dom';
+import 'typeface-roboto';
+import './wca_data/cubing-icons.css';
+import { getOauthTokenIfAny } from './logic/auth';
 
-import App from "./components/App/App";
+import App from './components/App/App';
 
 const userToken = getOauthTokenIfAny();
-ReactDOM.render(<App userToken={userToken} />, document.getElementById("root"));
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App userToken={userToken} />);
