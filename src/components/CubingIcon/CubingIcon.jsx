8890ef58dce54aee2eed29205de461211f96ca17
Gregor Billing <gregor.billing.dev@gmail.com>
2024-02-15 14:42:15 +0100

    Make sure CubingIcon understands the sx property

--- src/components/CubingIcon/CubingIcon.jsx
+++ src/components/CubingIcon/CubingIcon.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import classnames from 'classnames';
+import styled from '@emotion/styled';
+import { unstable_styleFunctionSx } from '@mui/system';
+
+const Span = styled('span')(unstable_styleFunctionSx);
 
 export const CubingIcon = ({ eventId, ...props }) => (
-  <span
+  <Span
     style={{ fontSize: 24 }}
     className={`cubing-icon event-${eventId}`}
     {...props}
@@ -10,7 +14,7 @@ export const CubingIcon = ({ eventId, ...props }) => (
 );
 
 export const CubingIconUnofficial = ({ eventId, className, ...props }) => (
-  <span
+  <Span
     style={{ fontSize: 24 }}
     className={classnames('cubing-icon', `unofficial-${eventId}`, className)}
     {...props}
