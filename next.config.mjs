a03c89730f14a6e6a163cc7555db6633a78a01c2
Gregor Billing <gregor.billing.dev@gmail.com>
2024-02-15 14:04:37 +0100

    Change build output folder back to NextJS default
    See https://github.com/vercel/next.js/issues/61905

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA).
-  distDir: './build', // Changes the build output directory to `./build/`.
 }
 
 export default nextConfig
